dc1156ec3c16eea777f259e924481eedf7c5f51a
Cem Başar Başkan <ideasoftware34@gmail.com>
2023-09-18 22:10:17 +0000

    Memento pattern added.

--- src/main/java/org/designpatterns/behavioral/memento/Demo.java
+++ src/main/java/org/designpatterns/behavioral/memento/Demo.java
@@ -1,4 +1,19 @@
 package org.designpatterns.behavioral.memento;
 
+// https://www.baeldung.com/java-memento-design-pattern
+// https://github.com/eugenp/tutorials/tree/master/patterns-modules/design-patterns-behavioral-2/src/main/java/com/baeldung/memento
 public class Demo {
+    public static void main(String[] args) {
+        TextEditor textEditor = new TextEditor(new TextWindow());
+        textEditor.write("The Memento Design Pattern\n");
+        textEditor.write("How to implement it in Java?\n");
+        textEditor.hitSave();
+
+        textEditor.write("Buy milk and eggs before coming home\n");
+
+        textEditor.hitUndo();
+
+        // System.out.println(textEditor.print().equals("The Memento Design Pattern\nHow to implement it in Java?\n"));
+        System.out.println(textEditor.print());
+    }
 }
---
+++ src/main/java/org/designpatterns/behavioral/memento/TextWindowState.java
@@ -0,0 +1,16 @@
+package org.designpatterns.behavioral.memento;
+
+// The Memento. 
+// As we can see, we choose to use String instead of StringBuilder to prevent any update of the current text by outsiders.
+public class TextWindowState {
+    private String text;
+
+    public TextWindowState(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+}
