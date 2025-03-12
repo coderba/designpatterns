9e2d273ccf58697bc437bc02ea8e0209972ee975
Cem Başar <ideasoftware34@gmail.com>
2025-03-12 08:45:41 +0000

    code space test

--- src/main/java/org/designpatterns/behavioral/strategy/Demo.java
+++ src/main/java/org/designpatterns/behavioral/strategy/Demo.java
@@ -2,6 +2,6 @@ package org.designpatterns.behavioral.strategy;
 
 public class Demo {
     public static void main(String... args){
-        System.out.println("hello world");
+        System.out.println("code space test");
     }
 }
