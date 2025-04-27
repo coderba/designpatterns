4a9ef6f74071aa2855eb4c79c6cabdabdb9a2389
Cem Başar <ideasoftware34@gmail.com>
2025-04-27 20:17:44 +0000

    test commit

--- src/main/java/org/designpatterns/behavioral/chainofresponsibility/Demo.java
+++ src/main/java/org/designpatterns/behavioral/chainofresponsibility/Demo.java
@@ -1,4 +1,7 @@
 package org.designpatterns.behavioral.chainofresponsibility;
 
 public class Demo {
+    public static void main(String... args){
+        System.out.println("hello world");
+    }
 }
