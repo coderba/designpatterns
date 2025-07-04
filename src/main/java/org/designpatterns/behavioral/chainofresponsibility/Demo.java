f1cead0eb62fa7ba25e2e1c50b9fa46a96042fd5
Cem Başar <ideasoftware34@gmail.com>
2025-07-04 08:13:49 +0000

    hello world from ipad

--- src/main/java/org/designpatterns/behavioral/chainofresponsibility/Demo.java
+++ src/main/java/org/designpatterns/behavioral/chainofresponsibility/Demo.java
@@ -3,5 +3,7 @@ package org.designpatterns.behavioral.chainofresponsibility;
 public class Demo {
     public static void main(String... args){
         System.out.println("hello world");
+        System.out.println("hello world again");
+        System.out.println("hello world again and again");
     }
 }
