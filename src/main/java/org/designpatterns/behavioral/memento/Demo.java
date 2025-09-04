c9a45abc5dfdc6b5241ca017edfcace0dd705965
Cem Başar <ideasoftware34@gmail.com>
2025-09-04 21:29:12 +0300

    MEMENTO PATTERN

--- src/main/java/org/designpatterns/behavioral/memento/Demo.java
+++ src/main/java/org/designpatterns/behavioral/memento/Demo.java
@@ -1,22 +1,36 @@
-//package org.designpatterns.behavioral.memento;
-//
-//import org.designpatterns.behavioral.memento.model.TextEditor;
-//import org.designpatterns.behavioral.memento.model.TextWindow;
-//
-//// https://www.baeldung.com/java-memento-design-pattern
-//// https://github.com/eugenp/tutorials/tree/master/patterns-modules/design-patterns-behavioral-2/src/main/java/com/baeldung/memento
-//public class Demo {
-//    public static void main(String[] args) {
-//        TextEditor textEditor = new TextEditor(new TextWindow());
-//        textEditor.write("The Memento Design Pattern\n");
-//        textEditor.write("How to implement it in Java?\n");
-//        textEditor.hitSave();
-//
-//        textEditor.write("Buy milk and eggs before coming home\n");
-//
-//        textEditor.hitUndo();
-//
-//        // System.out.println(textEditor.print().equals("The Memento Design Pattern\nHow to implement it in Java?\n"));
-//        System.out.println(textEditor.print());
-//    }
-//}
+package org.designpatterns.behavioral.memento;
+
+public class Demo {
+    public static void main(String[] args) {
+        org.designpatterns.behavioral.memento.TextEditor editor = new org.designpatterns.behavioral.memento.TextEditor("");
+        editor.addText("Initial content.");
+        History history = new History();
+
+        // Save the initial state
+        System.out.println("\n--- Performing an Save ---\n");
+        history.save(editor.save());
+
+        editor.addText(" Some new words.");
+
+        // Save the new state
+        System.out.println("\n--- Performing an Save ---\n");
+        history.save(editor.save());
+
+        editor.addText(" And more words.");
+
+        System.out.println("\n--- Performing an Undo ---\n");
+        // Undo the last change
+        TextEditorMemento lastState = history.undo();
+        if (lastState != null) {
+            editor.restore(lastState);
+        }
+
+        System.out.println("\n--- Performing another Undo ---\n");
+        // Undo the first change
+        lastState = history.undo();
+        if (lastState != null) {
+            editor.restore(lastState);
+        }
+
+    }
+}
